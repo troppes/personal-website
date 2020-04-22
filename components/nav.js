d97736321d4fd5763fb9387cadb068f29b3a5b97
Florian Reitz <flo@reitz.dev>
2020-04-22 17:36:12 +0200

    restarting

--- components/nav.js
+++ components/nav.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 export default function Nav() {
   return (
-    <header class="navbar">
-    <section class="navbar-section">
-        <Link href='/' as={ process.env.BACKEND_URL + '/'}><a class="btn btn-link">Home</a></Link>
-        <Link href='/blog' as={ process.env.BACKEND_URL + '/blog'}><a class="btn btn-link">Blog</a></Link>
+    <header className="navbar">
+    <section className="navbar-section">
+        <Link href='/' as={ process.env.BACKEND_URL + '/'}><a className="btn btn-link">Home</a></Link>
+        <Link href='/blog' as={ process.env.BACKEND_URL + '/blog'}><a className="btn btn-link">Blog</a></Link>
     </section>
-    <section class="navbar-center">
-      <Link href='/' as={ process.env.BACKEND_URL + '/'}><a class="btn btn-link">Nomi's Blog</a></Link>
+    <section className="navbar-center">
+      <Link href='/' as={ process.env.BACKEND_URL + '/'}><a className="btn btn-link">Nomi's Blog</a></Link>
     </section>
-    <section class="navbar-section">
-        <a href="#" class="btn btn-link">Twitter</a>
-        <a href="https://github.com/Nomi-1/" target='_blank'class="btn btn-link">GitHub</a>
+    <section className="navbar-section">
+        <a href="#" className="btn btn-link">Twitter</a>
+        <a href="https://github.com/Nomi-1/" target='_blank'className="btn btn-link">GitHub</a>
     </section>
     </header>
   )
--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import Nav from '../components/nav';
 import Head from '../components/head';
-import { getSortedPostsData } from '../lib/posts'
+
 export default () => (
   <div>
     <Head />
     <Nav />
-    Here is the blog.
   </div>
 )
+
+
