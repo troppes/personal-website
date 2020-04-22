6298e3727c2c9eecd3b90c1bd2688120e0da8c45
Florian Reitz <flo@reitz.dev>
2020-04-22 17:47:11 +0200

    added simple blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,35 @@
 import Link from 'next/link';
 import Nav from '../components/nav';
 import Head from '../components/head';
+import { getSortedPostsData } from '../lib/posts'
 
-export default () => (
-  <div>
-    <Head />
-    <Nav />
-  </div>
-)
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Blog({ allPostsData }) {
+  return (
+    <div>
+      <Head />
+      <Nav />
+      <ul className='test'>
+        {allPostsData.map(({ id, date, title }) => (
+          <li className='' key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 
