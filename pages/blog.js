1f418a1b95b318b00f899db8855280db73556220
Florian Reitz <flo@reitz.dev>
2020-04-22 16:28:38 +0200

    adding blog functionality

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Nav from '../components/nav';
 import Head from '../components/head';
+import { getSortedPostsData } from '../lib/posts'
 export default () => (
   <div>
     <Head />
