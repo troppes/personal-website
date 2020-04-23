8738cefd5ca7decb34160882d2a7c450b19d4722
Florian Reitz <flo@reitz.dev>
2020-04-23 11:09:55 +0200

    fixed logo for gh pages

--- components/nav.js
+++ components/nav.js
@@ -9,7 +9,7 @@ export default function Nav() {
         <Link href='/blog' as={ process.env.BACKEND_URL + '/blog'}><a className="btn btn-link">Blog</a></Link>
     </section>
     <section className="navbar-center">
-      <img src='/img/logo_dark.svg' alt='logo' as={ process.env.BACKEND_URL + '/img/logo_dark.svg'} width='50px' height='50px' /> 
+      <img src='/img/logo_dark.svg' as={ process.env.BACKEND_URL + '/img/logo_dark.svg'}  alt='logo' width='50px' height='50px' /> 
     </section>
     <section className="navbar-section">
         <a href="https://www.linkedin.com/in/florian-reitz-16a10b150/" className="btn btn-link">LinkedIn</a>
