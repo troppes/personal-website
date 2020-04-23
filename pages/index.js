2bf7cf8b266df3fdd2a7249d8420750baeeefde5
Florian Reitz <flo@reitz.dev>
2020-04-23 13:02:37 +0200

    added Blog Design

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
 import Head from '../components/head'
 import Nav from '../components/nav';
+import Layout from '../components/layout';
 export default () => (
   <div>
+    <Layout>
     <Head />
-    <Nav />
+      <div className="column col-12">
+        <Nav />
+      </div>
+      <div className="column col-6 col-mx-auto">
+         Content goes here.
+      </div>
+    </Layout>
   </div>
 );
