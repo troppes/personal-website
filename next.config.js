ba9d2388fde4dab5b4c347e76e3fd4b7670ae487
Florian Reitz <flo@reitz.dev>
2020-04-24 12:55:26 +0200

    removed comments and useless div

--- next.config.js
+++ next.config.js
@@ -11,12 +11,8 @@ module.exports = {
       "/blog": { page: "/blog" },
     }
   },
-  //assetPrefix: '',
   assetPrefix: !debug ? '/personal-website/' : '',
   webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
     config.module.rules = config.module.rules.map(rule => {
       if(rule.loader === 'babel-loader') {
         rule.options.cacheDirectory = false
@@ -25,12 +21,5 @@ module.exports = {
     })
     // Important: return the modified config
     return config
-  }/*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
+  }
 } 
--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,61 @@ import Head from '../components/head'
 import Nav from '../components/nav';
 import Layout from '../components/layout';
 export default () => (
-  <div>
-    <Layout>
-      <Head />
-      <div className="column col-12">
-        <Nav />
-      </div>
-      <div className="column col-12 mt-2 col-mx-auto">
-        <div className="columns">
-          <div className="column col-3 col-md-6 mt-2 col-ml-auto">
-            <h2>Florian Reitz</h2>
-            <p>Hello, my name is Florian. I currently study computer science at HTW Berlin. For more information see my LinkedIn.
+  <Layout>
+    <Head />
+    <div className="column col-12">
+      <Nav />
+    </div>
+    <div className="column col-12 mt-2 col-mx-auto">
+      <div className="columns">
+        <div className="column col-3 col-md-6 mt-2 col-ml-auto">
+          <h2>Florian Reitz</h2>
+          <p>Hello, my name is Florian. I currently study computer science at HTW Berlin. For more information see my LinkedIn.
           </p>
-          </div>
-          <div className="column col-2 col-md-6 mt-2 col-mr-auto">
-            <img src={process.env.BACKEND_URL + '/img/me.png'} alt='logo' className='img-responsive' style={{'border-radius': 50 + '%'}}/>
-          </div>
+        </div>
+        <div className="column col-2 col-md-6 mt-2 col-mr-auto">
+          <img src={process.env.BACKEND_URL + '/img/me.png'} alt='logo' className='img-responsive' style={{ 'border-radius': 50 + '%' }} />
         </div>
       </div>
-      <div className="column col-12 col-mx-auto">
-        <div className="column col-5 col-md-12 mt-2 col-mx-auto">
-          <table role="table" className='table'>
-            <thead>
-              <tr>
-                <th role="columnheader">Skill</th>
-                <th role="columnheader">Level</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr role="row">
-                <td role="cell">Spanish</td>
-                <td role="cell"><p>Beginner</p>
-                </td>
-              </tr>
-              <tr role="row">
-                <td role="cell">English</td>
-                <td role="cell"><p>Expert</p>
-                </td>
-              </tr>
-              <tr role="row">
-                <td role="cell">DevOps</td>
-                <td role="cell"><p>Proficient</p>
-                </td>
-              </tr>
-              <tr role="row">
-                <td role="cell">HTML, CSS, PHP, Javascript, NodeJS, Symfony</td>
-                <td role="cell"><p>Proficient</p>
-                </td>
-              </tr>
-              <tr role="row">
-                <td role="cell">Java</td>
-                <td role="cell"><p>Proficient</p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    </div>
+    <div className="column col-12 col-mx-auto">
+      <div className="column col-5 col-md-12 mt-2 col-mx-auto">
+        <table role="table" className='table'>
+          <thead>
+            <tr>
+              <th role="columnheader">Skill</th>
+              <th role="columnheader">Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr role="row">
+              <td role="cell">Spanish</td>
+              <td role="cell"><p>Beginner</p>
+              </td>
+            </tr>
+            <tr role="row">
+              <td role="cell">English</td>
+              <td role="cell"><p>Expert</p>
+              </td>
+            </tr>
+            <tr role="row">
+              <td role="cell">DevOps</td>
+              <td role="cell"><p>Proficient</p>
+              </td>
+            </tr>
+            <tr role="row">
+              <td role="cell">HTML, CSS, PHP, Javascript, NodeJS, Symfony</td>
+              <td role="cell"><p>Proficient</p>
+              </td>
+            </tr>
+            <tr role="row">
+              <td role="cell">Java</td>
+              <td role="cell"><p>Proficient</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </Layout>
-  </div>
+    </div>
+  </Layout>
 );
