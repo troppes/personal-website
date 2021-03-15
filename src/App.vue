3ff9fc7a51a76c1aca994d297947c626cead0e23
Florian Reitz <flo@reitz.dev>
2021-03-15 17:25:34 +0100

    fixed formatting for eslint

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="p-6 max-w-sm mx-auto bg-white rounded-xl flex items-center space-x-4"/>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+    class="p-6 max-w-sm mx-auto bg-white rounded-xl flex items-center space-x-4"
+  >
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <Notification />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import Notification from './components/Notification.vue'
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Notification from './components/Notification.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
-    Notification
-  }
-})
+    Notification,
+  },
+});
 </script>
