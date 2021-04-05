97a097c917183bb6bec70659ef40fd82182288d6
Florian Reitz <flo@reitz.dev>
2021-04-05 12:14:38 +0200

    added project cards

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
   <Header />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <Notification />
+  <Card
+    msg="Hallo this is a card"
+    title="Hello World"
+  />
+  <Card
+    msg="Hallo this is a card"
+    title="Hello World"
+    text-upper-left="Test"
+  />
   <Footer />
 </template>
 
@@ -13,10 +22,12 @@ import Notification from './components/Notification.vue';
 import Header from './components/Header.vue';
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import Card from './components/Card.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    Card,
     Footer,
     Navbar,
     Header,
---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="flex flex-col justify-between w-72 sm:w-96 h-96 bg-white bg-center text-gray-800
+     shadow-md overflow-hidden cursor-pointer rounded-xl hover:shadow-xl"
+    style="background-image:url('https://placeimg.com/480/480/any')"
+  >
+    <div class="flex justify-between items-center ml-4 pr-8">
+      <div
+        v-if="textUpperLeft"
+        class="bg-red-600 text-white bg-opacity-95 shadow px-2 py-1 flex items-center font-bold text-xs rounded"
+      >
+        {{ textUpperLeft }}
+      </div>
+      <div
+        v-if="textUpperRight"
+        class="bg-red-600 w-10 h-12 shadow flex flex-col-reverse p-2 text-center font-bold text-white rounded-b-full"
+      >
+        {{ textUpperRight }}
+      </div>
+    </div>
+    <div class="bg-white bg-opacity-95 shadow-md rounded-r-xl p-4 flex flex-col mr-4 mb-8">
+      <h3 class="text-xl font-bold pb-2">
+        {{ title }}
+      </h3>
+      <p class="truncate text-gray-500 text-sm">
+        {{ msg }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    msg: {
+      type: String,
+      required: true,
+    },
+    textUpperLeft: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    textUpperRight: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
