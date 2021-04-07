222e60b312997c7ddb8a8a5cb2f5cef3a10f339a
Florian Reitz <flo@reitz.dev>
2021-04-07 16:36:43 +0200

    big refactor; made components dumb; markedtypes with Type suffix; change card to project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <Navbar />
   <router-view />
-  <Footer />
+  <!-- <Footer /> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Footer,
     Navbar,
   },
 });
