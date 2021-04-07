593d753cf7664625fe1eb20f1836de248e0f8e97
Florian Reitz <flo@reitz.dev>
2021-04-07 18:23:25 +0200

    removed footer only for blog

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <Navbar />
   <router-view />
-  <!-- <Footer /> -->
+  <Footer v-if="!$route.meta.hideFooter" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    Footer,
     Navbar,
   },
 });
