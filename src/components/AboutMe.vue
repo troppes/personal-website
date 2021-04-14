275a5434489595c88b19a30c24d68bc3775ea58d
Florian Reitz <flo@reitz.dev>
2021-04-14 16:05:12 +0200

    fixed about me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="about-me test">
-    <div class="photo">
-      <img
-        class="left pure-img"
-        alt="profile picture"
-        :src="'https://cms.reitz.dev/assets/'+pictureKey"
-      >
+  <div class="pure-g">
+    <div class="pure-u-lg-4-24" />
+    <div class="about-me pure-u-lg-16-24 pure-g">
+      <div class="photo pure-u-1 pure-u-xl-1-3">
+        <img
+          class="left pure-img"
+          alt="profile picture"
+          :src="'https://cms.reitz.dev/assets/'+pictureKey"
+        >
+      </div>
+      <div class="content pure-u-1 pure-u-xl-1-3">
+        <h1>{{ title }}</h1>
+        <div class="separator" />
+        <p v-html="msg" />
+        <social-media
+          size="60"
+          class="social-media"
+        />
+      </div>
     </div>
-    <div class="right">
-      <h1>{{ title }}</h1>
-      <div class="separator" />
-      <p v-html="msg" />
-      <social-media
-        size="60"
-        class="social-media"
-      />
-    </div>
+    <div class="pure-u-lg-4-24" />
   </div>
 </template>
 
@@ -48,60 +52,23 @@ export default {
 <style scoped>
 /* The card */
 .about-me {
-  position: relative;
-  height: 50vh;
-  width: 100vh;
-  margin: 5vh auto;
   background-color: var(--secondary-color);
   border: 2px solid var(--colour-background);
 }
 
-.about-me:before{
-  content: "";
-  position: absolute;
-  top: calc(var(--base-grid) * -2);
-  left: calc(var(--base-grid) * -2);
-  border: 2px dashed var(--colour-background);
-  background-image: repeating-linear-gradient(-24deg, transparent, transparent 4px, var(--colour-background-stripes) 0,
-  var(--colour-background-stripes) 5px);
-  z-index: -1;
-  width: calc(var(--base-grid) * 110);
-  height: calc(var(--base-grid) * 55);
+.photo {
+  position: relative;
+  top: -8vh;
+  margin-bottom: -8vh;
+  overflow: hidden;
 }
 
-/* Image on the left side */
-.photo {
-  float: left;
-  position: relative;
-  left: 5vh;
-  top: -4vh;
-  height: 30vh;
-  width: 50vh;
-  overflow: hidden;
+.photo img{
   border: 2px solid var(--colour-background);
 }
 
-/*object-fit: cover;*/
-/*object-position: center;*/
-img.left {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  height: auto;
-  width: 100%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-}
-
-/* Right side of the card */
-.right {
-  margin-left: 590px;
-  margin-right: 20px;
-}
-
 .separator {
-  margin-top: 10px;
+  margin-top: 5vh;
   border: 1px solid #C3C3C3;
 }
 
@@ -113,7 +80,44 @@ p {
 }
 
 .social-media {
-  padding-top: 10vh;
+  padding-top: 2vh;
+}
+
+@media (min-width: 80em) {
+  .photo {
+    float: left;
+    position: relative;
+    left: 2vh;
+    top: -8vh;
+    height: 30vh;
+    width: 50vh;
+    overflow: hidden;
+    border: 2px solid var(--colour-background);
+  }
+
+  .content {
+    float:right;
+    margin-right: 2vh;
+    width: 40vh;
+  }
+
+  /*object-fit: cover;*/
+  /*object-position: center;*/
+  img.left {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    height: auto;
+    width: 100%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+
+  .social-media {
+    padding-top: 10vh;
+  }
+
 }
 
 </style>
