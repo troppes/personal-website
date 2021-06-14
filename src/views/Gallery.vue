1ed9b5099588e6345c7eb357155aa88167e29bc9
Florian Reitz <flo@reitz.dev>
2021-06-14 12:32:25 +0200

    switched to lightgallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,43 +3,45 @@
     :image-key="galleryHeader.imageKey"
     :text="galleryHeader.text"
   />
-  <div id="gallery">
-    <GalleryPhoto
+  <light-gallery
+    :settings="{ speed: 500, plugins: plugins }"
+    :on-init="onInit"
+  >
+    <a
       v-for="photo in galleryPhotos"
       :key="photo.id"
-      :photo="photo"
-      @open-photo="openPhoto"
-    />
-  </div>
-  <teleport to="body">
-    <div
-      v-if="isModalVisible"
-      class="modal-wrapper"
-      @click="isModalVisible = false"
+      data-lg-size="1406-1390"
+      class="gallery-item"
+      :data-src="'https://cms.reitz.dev/assets/'+photo.photoKey"
+      :data-sub-html="photo.description"
     >
-      <div class="modal">
-        <img
-          :src="modalImage"
-          width="400"
-        >
-      </div>
-    </div>
-  </teleport>
+      <img
+        :alt="photo.alt"
+        class="img-responsive"
+        :src="'https://cms.reitz.dev/assets/'+photo.photoKey+'?fit=cover&width=800&quality=80'"
+      >
+    </a>
+  </light-gallery>
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from 'vue';
 import axios from 'axios';
+import LightGallery from 'lightgallery/vue';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
 import GalleryHeaderType from '../types/GalleryHeaderType';
 import GalleryHeader from '../components/gallery/GalleryHeader.vue';
 import GalleryPhotoType from '../types/GalleryPhotoType';
-import GalleryPhoto from '../components/gallery/GalleryPhoto.vue';
+
+let lightGallery: any = null;
 
 export default defineComponent({
   name: 'App',
   components: {
     GalleryHeader,
-    GalleryPhoto,
+    LightGallery,
   },
   data() {
     return {
@@ -47,16 +49,23 @@ export default defineComponent({
       galleryHeader: { text: '', imageKey: '' },
       isModalVisible: false,
       modalImage: '',
+      plugins: [lgThumbnail, lgZoom],
     };
+  },
+  watch: {
+    galleryPhotos() {
+      this.$nextTick(() => {
+        lightGallery.refresh();
+      });
+    },
   },
   mounted() {
     this.fetchGalleryHeader();
     this.fetchGallery();
   },
   methods: {
-    openPhoto(photo: GalleryPhotoType) {
-      this.modalImage = `https://cms.reitz.dev/assets/${photo.photoKey}`;
-      this.isModalVisible = true;
+    onInit: (detail: any) => {
+      lightGallery = detail.instance;
     },
     async fetchGalleryHeader() {
       try {
@@ -116,33 +125,4 @@ h2 {
   text-align: center;
   font-size: 20vh;
 }
-
-.modal-wrapper {
-  z-index: 20;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, .5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal {
-  background: #FFFFFF;
-  box-shadow: 2px 2px 20px 1px;
-  overflow-x: auto;
-  display: flex;
-  flex-direction: column;
-  transition: opacity .3s ease;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-
 </style>
