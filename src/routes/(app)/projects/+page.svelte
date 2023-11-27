f46955ca1509192f38d2d640f35398a676166388
Florian Reitz <flo@reitz.dev>
2023-11-27 18:53:45 +0100

    fixed projects page

--- src/routes/(app)/projects/+page.svelte
+++ src/routes/(app)/projects/+page.svelte
@@ -3,14 +3,34 @@
 	export let data;
 </script>
 
-<div class="pure-g cards">
+<div class="spacer" />
+<div class="grid-container">
 	{#each data.projects as post}
-		<Card {post} />
+		<div class="card-wrapper">
+			<Card {post} />
+		</div>
 	{/each}
 </div>
 
 <style>
-	.cards {
-		padding-top: 10vh;
+	.spacer {
+		padding-top: 15vh;
+	}
+	.grid-container {
+		display: grid;
+		row-gap: 20px;
+		grid-template-columns: repeat(1, 1fr);
+	}
+
+	@media only screen and (min-width: 768px) {
+		.grid-container {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+
+	@media only screen and (min-width: 1200px) {
+		.grid-container {
+			grid-template-columns: repeat(4, 1fr);
+		}
 	}
 </style>
