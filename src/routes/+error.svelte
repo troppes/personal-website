df73fac05964878e432b52ebf9cc504c694ef2fe
Florian Reitz <flo@reitz.dev>
2023-11-27 18:30:32 +0100

    fixed 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<h1 class="code">404</h1>
+<div class="message">
+	<p>That should not happen! The page you are looking for does not exist.</p>
+</div>
+
+<style>
+	.code,
+	.message {
+		padding-top: 8vh;
+		padding-bottom: 1dvb;
+		text-align: center;
+	}
+</style>
