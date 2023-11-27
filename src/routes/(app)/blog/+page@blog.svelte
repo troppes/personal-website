c9c5ba059a2ec18d625b8340e740acf2fb04a24f
Florian Reitz <flo@reitz.dev>
2023-11-27 19:31:20 +0100

    fixed blog

--- src/routes/(app)/blog/+page@blog.svelte
+++ src/routes/(app)/blog/+page@blog.svelte
@@ -5,21 +5,19 @@
 	export let data;
 </script>
 
-<div id="layout" class="pure-g">
-	<div class="sidebar pure-u-1 pure-u-md-1-4">
-		<div class="header">
-			<h1 class="brand-title">
-				{data.metadata.data.title}
-			</h1>
-			<div class="brand-tagline">
-				{@html data.metadata.data.tagline}
-			</div>
-			<div class="social-media">
-				<SocialMedia socialMedia={data.socialMedia} />
-			</div>
-		</div>
+<div class="sidebar">
+	<h1 class="brand-title">
+		{data.metadata.data.title}
+	</h1>
+	<div class="brand-tagline">
+		{@html data.metadata.data.tagline}
 	</div>
-	<div class="content pure-u-1 pure-u-md-3-4">
+	<div class="social-media">
+		<SocialMedia socialMedia={data.socialMedia} />
+	</div>
+</div>
+<div class="flex-container">
+	<div class="content">
 		{#each data.posts as post}
 			<BlogPost {post} />
 		{/each}
@@ -29,18 +27,11 @@
 </div>
 
 <style>
-	#layout {
-		padding-top: 8vh;
-	}
-
-	.header {
+	.sidebar {
+		margin-top: 15vh;
 		text-align: center;
 		top: auto;
 		margin: 3em auto;
-	}
-
-	.sidebar {
-		background: var(--secondary-color);
 	}
 
 	.brand-title,
@@ -50,6 +41,7 @@
 
 	.brand-title {
 		text-transform: uppercase;
+		font-size: 3em;
 	}
 
 	.brand-tagline {
@@ -61,22 +53,23 @@
 		margin-top: 2vh;
 	}
 
-	@media (min-width: 48em) {
+	@media (min-width: 1024px) {
 		.content {
 			float: left;
 			padding: 2em 3em 0;
 			margin-left: 25%;
 		}
 
-		.header {
-			margin: 80% 2em 0;
-		}
-
 		.sidebar {
-			margin-top: 8vh;
-			position: absolute;
-			top: 0;
-			bottom: 0;
+			border-radius: 5px;
+			margin-top: 20vh;
+			padding: 10vh 2vh 10vh 2vh;
+			width: 400px; /* Set the width of the sidebar */
+			position: absolute; /* Fixed Sidebar (stay in place on scroll) */
+			z-index: 1; /* Stay on top */
+			top: 0; /* Stay at the top */
+			left: 0;
+			background-color: var(--secondary-color);
 		}
 	}
 </style>
--- src/routes/(app)/blog/[slug]/+page@(app).svelte
+++ src/routes/(app)/blog/[slug]/+page@(app).svelte
@@ -3,26 +3,18 @@
 </script>
 
 <div class="content blog">
-	<div class="pure-u-xl-4-24" />
-	<div class="pure-g center pure-u-xl-16-24">
-		<div class="pure-u-1">
-			<h2>{data.post.data.title}</h2>
-		</div>
-		<div class="pure-u-1">
-			<p>{@html data.post.data.text}</p>
-		</div>
+	<div class="flex-container">
+		<h2>{data.post.data.title}</h2>
+
+		<p>{@html data.post.data.text}</p>
 	</div>
-	<div class="pure-u-xls-4-24" />
 </div>
 
 <style>
 	.content {
+		padding-left: 10vh;
+		padding-right: 10vh;
 		padding-top: 10vh;
-		padding-bottom: 10vh;
 		color: var(--secondary-text-color);
-	}
-
-	.center {
-		background-color: var(--secondary-color);
 	}
 </style>
