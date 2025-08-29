f316bb6625747a33767c2e4a96ff7d0bbe3f1a12
Florian <flo@reitz.dev>
2025-08-29 19:25:25 +0200

    fixed eslint config

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("eslint/config");
+
+module.exports = defineConfig([
+  {
+    ignores: [
+      ".DS_Store",
+      "node_modules",
+      "/build",
+      "/.svelte-kit",
+      "/package",
+      ".env",
+      ".env.*",
+      "!/.env.example",
+      "pnpm-lock.yaml",
+      "package-lock.json",
+      "yarn.lock",
+    ],
+    root: true,
+    extends: ["eslint:recommended", "plugin:svelte/recommended", "prettier"],
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: 2022,
+      extraFileExtensions: [".svelte"],
+    },
+    env: {
+      browser: true,
+      es2022: true,
+      node: true,
+    },
+  },
+]);
